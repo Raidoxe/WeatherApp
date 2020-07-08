620e16f5ed57a46c3faa2debea3afaa8e2e7c65a
Griffin_Jones <jonesg873@gmail.com>
2020-07-08 16:02:53 +0800

    added Component to load the day

---
+++ components/LoadDay.js
@@ -0,0 +1,14 @@
+const loadDay = (props) => {
+    return(
+        <div className="week-day">
+            <h4 className="day-one">Thursday</h4>
+            <div className="high-low-temp">
+                <p className="temp-icon-lg">Icon</p>
+                <p className="temp-high">{props.max}°</p>
+                <p className="temp-low">{props.min}°</p>
+            </div>
+        </div>
+    )
+}
+
+export default loadDay;
