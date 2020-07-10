fdaf5f9d3ffa71e6802ee8a8472c3c32a859cd00
David <davidfrenchy89@gmail.com>
2020-07-10 10:41:08 +0800

    Css to icons

--- components/LoadDay.js
+++ components/LoadDay.js
@@ -5,7 +5,7 @@ const loadDay = (props) => {
         <div className="week-day">
             <h4 className="day-one">{props.day}</h4>
             <div className="high-low-temp">
-                <p className="temp-icon-lg"><img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt="React Logo" /></p>
+                <p className="temp-icon-lg"><img className="lg-icon" src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt="React Logo" /></p>
                 <p className="temp-high">{props.max}°</p>
                 <p className="temp-low">{props.min}°</p>
             </div>
